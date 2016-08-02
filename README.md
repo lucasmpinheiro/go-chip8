b30309101484a9c261b11343403de2014b901b91
LucasMP <lucasmpinheiro@gmail.com>
2016-08-02 00:39:14 -0300

    Implement method to decode opcodes (work in progress).

--- README.md
+++ README.md
@@ -3,3 +3,46 @@
 This is a project to create a simple Chip 8 emulator written in [Go](https://golang.org/).
 
 It's largely based on the [tutorial by Laurance Muller](http://www.multigesture.net/articles/how-to-write-an-emulator-chip-8-interpreter/).
+
+## TODO
+
+### Opcodes
+- [ ] 0x0000
+    - [ ] 0x0NNN
+    - [ ] 0x00E0
+    - [ ] 0x00EE
+- [x] 0x1NNN
+- [x] 0x2NNN
+- [x] 0x3XNN
+- [x] 0x4XNN
+- [x] 0x5XY0
+- [x] 0x6XNN
+- [x] 0x7XNN
+- [ ] 0x8000
+    - [ ] 0x8XY0
+    - [ ] 0x8XY1
+    - [ ] 0x8XY2
+    - [ ] 0x8XY3
+    - [ ] 0x8XY4
+    - [ ] 0x8XY5
+    - [ ] 0x8XY6
+    - [ ] 0x8XY7
+    - [ ] 0x8XYE
+- [x] 0x9XY0
+- [x] 0xANNN
+- [x] 0xBNNN
+- [ ] 0xCXNN
+- [ ] 0xDXYN
+- [ ] 0xE000
+    - [ ] 0xEX9E
+    - [ ] 0xEXA1
+- [ ] 0xF000:
+    - [x] 0xFX07
+    - [ ] 0xFX0A
+    - [x] 0xFX15
+    - [x] 0xFX18
+    - [x] 0xFX1E
+    - [ ] 0xFX29
+    - [ ] 0xFX33
+    - [x] 0xFX55
+    - [x] 0xFX65
--- system/core.go
+++ system/core.go
@@ -2,6 +2,7 @@ package core
 
 import (
 	"io/ioutil"
+	"log"
 )
 
 // Chip8 defines the system's main struct.
@@ -64,6 +65,161 @@ func (chip8 *Chip8) EmulateCycle() {
 	// Execute opcode.
 
 	// Update timers.
+
+// DecodeOpcode decodes the current opcode and performs the right operation.
+func (chip8 *Chip8) DecodeOpcode() {
+	switch chip8.opcode & 0xF000 {
+
+	case 0x0000:
+		switch chip8.opcode & 0x0FFF {
+		case 0x00E0: // 0x00E0: Clears the screen.
+			// TODO: implement this
+		case 0x00EE: // 0x00EE: Returns from a subroutine
+			// TODO: implement this
+		default:
+			log.Fatalln("Unknown opcode [0x0000]: %X", chip8.opcode)
+		}
+
+	case 0x1000: // 0x1NNN: Jumps to address NNN.
+		chip8.pc = chip8.opcode & 0x0FFF
+
+	case 0x2000: // 0x2NNN: Calls subroutine at NNN.
+		chip8.stack[chip8.sp] = chip8.pc
+		chip8.sp++
+		chip8.pc = chip8.opcode & 0x0FFF
+
+	case 0x3000: // 0x3XNN: Skips the next instruction if VX equals NN.
+		x := (chip8.opcode & 0x0F00) / 100
+		if chip8.V[x] == uint8(chip8.opcode&0x00FF) {
+			chip8.pc += 4
+		} else {
+			chip8.pc += 2
+		}
+
+	case 0x4000: // 0x4XNN: Skips the next instruction if VX doesn't equal NN.
+		x := (chip8.opcode & 0x0F00) / 100
+		if chip8.V[x] != uint8(chip8.opcode&0x00FF) {
+			chip8.pc += 4
+		} else {
+			chip8.pc += 2
+		}
+
+	case 0x5000: // 0x5XY0: Skips the next instruction if VX equals VY.
+		x := (chip8.opcode & 0x0F00) / 100
+		y := (chip8.opcode & 0x00F0) / 10
+		if chip8.V[x] == chip8.V[y] {
+			chip8.pc += 4
+		} else {
+			chip8.pc += 2
+		}
+
+	case 0x6000: // 0x6XNN:	Sets VX to NN.
+		x := (chip8.opcode & 0x0F00) / 100
+		chip8.V[x] = uint8(chip8.opcode & 0x00FF)
+		chip8.pc += 2
+
+	case 0x7000: // 0x7XNN:	Adds NN to VX.
+		x := (chip8.opcode & 0x0F00) / 100
+		chip8.V[x] += uint8(chip8.opcode & 0x00FF)
+		chip8.pc += 2
+
+	case 0x8000:
+		switch chip8.opcode & 0x000F {
+		case 0x0000:
+		case 0x0001:
+		case 0x0002:
+		case 0x0003:
+		case 0x0004:
+		case 0x0005:
+		case 0x0006:
+		case 0x0007:
+		case 0x000E:
+		default:
+			log.Fatalln("Unknown opcode [0x8000]: %X", chip8.opcode)
+		}
+
+	case 0x9000: // 0x9XY0: Skips the next instruction if VX doesn't equal VY.
+		x := (chip8.opcode & 0x0F00) / 100
+		y := (chip8.opcode & 0x00F0) / 10
+		if chip8.V[x] != chip8.V[y] {
+			chip8.pc += 4
+		} else {
+			chip8.pc += 2
+		}
+
+	case 0xA000: // ANNN: sets I to address NNN
+		chip8.I = chip8.opcode & 0x0FFF
+		chip8.pc += 2
+
+	case 0xB000: // 0xBNNN: Jumps to the address NNN plus V0.
+		chip8.pc = (chip8.opcode & 0x0FFF) + uint16(chip8.V[0])
+
+	case 0xC000:
+
+	case 0xD000:
+
+	case 0xE000:
+		switch chip8.opcode & 0x00FF {
+		case 0x009E:
+		case 0x00A1:
+		default:
+			log.Fatalln("Unknown opcode [0xE000]: %X", chip8.opcode)
+		}
+
+	case 0xF000:
+		switch chip8.opcode & 0x00FF {
+		case 0x0007: // 0xFX07: Sets VX to the value of the delay timer.
+			x := (chip8.opcode & 0x0F00) / 100
+			chip8.V[x] = chip8.delayTimer
+			chip8.pc += 2
+
+		case 0x000A:
+		case 0x0015: // 0xFX15: Sets the delay timer to VX.
+			x := (chip8.opcode & 0x0F00) / 100
+			chip8.delayTimer = chip8.V[x]
+			chip8.pc += 2
+		case 0x0018: // 0xFX15: Sets the sound timer to VX.
+			x := (chip8.opcode & 0x0F00) / 100
+			chip8.soundTimer = chip8.V[x]
+			chip8.pc += 2
+		case 0x001E: // 0xFX1E: Adds VX to I.
+			x := (chip8.opcode & 0x0F00) / 100
+			if chip8.I+uint16(chip8.V[x]) > 0xFFF { // Check for range overflow.
+				chip8.V[0xF] = 1
+			} else {
+				chip8.V[0xF] = 0
+			}
+			chip8.I += uint16(chip8.V[x])
+			chip8.pc += 2
+		case 0x0029:
+		case 0x0033:
+		case 0x0055: // 0xFX55: Stores V0 to VX (including VX) in memory starting at address I.
+			x := (chip8.opcode & 0x0F00) / 100
+			if chip8.I+x > uint16(len(chip8.memory)) {
+				log.Fatal("Error: end of memory.")
+			} else {
+				for i := uint16(0); i <= x; i++ {
+					chip8.memory[chip8.I+i] = chip8.V[i]
+				}
+			}
+
+		case 0x0065: // 0xFX65: Fills V0 to VX (including VX) with values from memory starting at address I.
+			x := (chip8.opcode & 0x0F00) / 100
+			if chip8.I+x > uint16(len(chip8.memory)) {
+				log.Fatal("Error: end of memory.")
+			} else {
+				for i := uint16(0); i <= x; i++ {
+					chip8.V[i] = chip8.memory[chip8.I+i]
+				}
+			}
+
+		default:
+			log.Fatalln("Unknown opcode [0xF000]: %X", chip8.opcode)
+		}
+
+	default:
+		log.Fatalln("Unknown opcode: %X", chip8.opcode)
+	}
 }
 
 // LoadGame loads the program stored on a file to the memory.
